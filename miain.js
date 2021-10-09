6c4f0348a9e78701d3b8ce271068ad71e39ce330
Gaurang-WEB-MAKER-OP <81744542+Gaurang-WEB-MAKER-OP@users.noreply.github.com>
2021-10-09 13:00:07 +0530

    Add files via upload

---
+++ miain.js
@@ -0,0 +1,43 @@
+webcam.set({
+    width: 350,
+    height: 300,
+    Image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+webcam.attach('#camera')
+
+
+
+function take_snapshot() {
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id ="captured_image" src="' + data_uri + '">';
+    });
+};
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('model loaded!')
+
+}
+
+function check() {
+    img = document.getElementById('selfie_image');
+
+}
+
+function gotResult(error, results) {
+
+    if (error) {
+        console.error(error);
+    } else {
+        console.log("result");
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+    }
+}
